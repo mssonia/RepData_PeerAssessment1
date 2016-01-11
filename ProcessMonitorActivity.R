7c1d3147fd7429ccc10818c4b59f9c608ebbaa20
Sonia V. Smith <sonia_smith@verizon.net>
2016-01-11 02:26:13 -0500

    Initial submission of course project 1 code and markdown.

--- ProcessMonitorActivity.R
+++ ProcessMonitorActivity.R
@@ -1,33 +1,12 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-output: 
-  html_document:
-    keep_md: true
----
-
-
-## Loading and preprocessing the data
-
-To begin our analysis, we must load the activity monitor dataset, stored in CSV format, into the global environment.
-
-```{r, echo=TRUE}
 library(dplyr)
 library(ggplot2) 
 
 # Read the activity CSV file to dataframe
 activityMonitorData <- read.csv(file="activity.csv", header=TRUE, sep=",")
 
-```
-
-The personal activity monitor data is now available for analysis using R functions generated to answer the questions identify patterns and trends pertaining to personal movement during 5 minute intervals.
-
-## What is mean total number of steps taken per day?
-
-The mean total number of steps taken per day is 9354.  The median total number of steps per day is 10395.
-
-This value was calculated by grouping the personal movement data by date and aggregating on the steps.  Once the data is aggregated, the mean and median of the steps is calculated.  A histogram was generated using ggplot to visualize the steps taken each day. The calculated mean and median is used by ggplot to draw a lines representing these values respectively.
-
-```{r, echo=FALSE}
+# ________________________________________________________________________________________________________________________
+# Answer to question "What is mean total number of steps taken per day?"
+# ________________________________________________________________________________________________________________________
 totalNumberSteps <- aggregate(activityMonitorData[c("steps")], by=activityMonitorData[c("date")], FUN=sum, na.rm=TRUE)  
 
 # Calculate and report the mean and median of the total number of steps taken per day
@@ -45,14 +24,9 @@ ggplot(totalNumberSteps, aes(x = steps)) +
 # Save plot ttlStepsTakenEachDayPlot
 ggsave(filename="ttlStepsTakenEachDayPlot.png", height=4, width=4)
 
-```
-
-
-## What is the average daily activity pattern?
-
-The average daily activity pattern is presented in the time series plot below. This plot observes the average number of steps taken each day at 5-minute intervals.  The average was calculated across all days.  The maximum number of steps is 835.  The red vertical line represents the maximum number of step on the graph.
-
-```{r, echo=TRUE}
+# ________________________________________________________________________________________________________________________
+# Answer to question "What is the average daily activity pattern?"
+# ________________________________________________________________________________________________________________________
 activityPattern <- aggregate(activityMonitorData[c("steps")], by=activityMonitorData[c("interval")], FUN=mean, na.rm=TRUE) 
 
 #  Which 5-minute interval, on average across all the days in the dataset, contains the maximum number of steps?
@@ -69,18 +43,10 @@ ggplot(activityPattern, aes(interval,steps)) +
 # Save plot timeSeriesStepIntrvlPlot.png
 ggsave(filename="timeSeriesStepIntrvlPlot.png", height=4, width=4)
 
-```
+# ________________________________________________________________________________________________________________________
+# Imputing missing values
+# ________________________________________________________________________________________________________________________
 
-
-## Imputing missing values
-
-The presence of missing days has the potential of introducing bias when performing some calculations and/or generating data summaries.  To avoid bia, implemented a strategy to update steps with value of NA with the result of mean of total steps/median of total step ( 0.899 = 9354/10395).
-
-The total number of missing values in the dataset is 2304.
-The mean of total steps is 9354.
-The median of total steps is 10395.
-
-```{r, echo=TRUE}
 # Calculate and report the total number of missing values in the dataset (i.e. the total number of rows with 𝙽𝙰s)
 totalNbrRowMissingSteps <- sum(is.na(activityMonitorData$steps))
 
@@ -108,16 +74,11 @@ ggplot(imputedTotalNumberSteps, aes(x = steps)) +
 # Save plot imputedTtlStepsTakenEachDayPlot
 ggsave(filename="imputedTtlStepsTakenEachDayPlot.png", height=4, width=4)
 
-```
 
+# ________________________________________________________________________________________________________________________
+# Answer to question "Are there differences in activity patterns between weekdays and weekends?"
+# ________________________________________________________________________________________________________________________
 
-## Are there differences in activity patterns between weekdays and weekends?
-
-To determine if there are differences in activity patterns, an additional column was added to the imputed activity monitor data to indicate whether the activity data was recorded during the week or during the weekend.  A panel plot was generated using ggplot presenting the data in time series plot recording 5-minute intervals and the average number of step taken accross all days occurring during the week or week's end. 
-
-The time series plots shows step activity began to peak during the weekday at 50 steps, 510 interval and reached its peak at 205 steps, 800 interval. Weekend activity pattern indicate step activity did not begin to peak until 25 steps, 750 interval and reached it peak at 155 steps, 900 interval.  Interestingly, weekend step activity spiked more frequently than weekday activity.   
-
-```{r, echo=TRUE}
 # Create a new factor variable in the dataset with two levels – “weekday” and “weekend” indicating whether a given date is a weekday or weekend day.
 dayTypeActivityPattern <- imputedActivityMonitorData
 dayTypeActivityPattern$daytype <- ifelse(weekdays(as.Date(dayTypeActivityPattern$date)) == "Saturday" | 
@@ -138,6 +99,3 @@ ggplot(dayTypeActivityPattern, aes(interval, steps),) +
 
 # Save plot dayTypeActivityPatternPlot
 ggsave(filename="dayTypeActivityPatternPlot.png", height=4, width=4)
-
-```
-
